3ee20eb268acb88e916e188fff2aea1f3dba1414
Nazmul Hossain <hossain-bd@users.noreply.github.com>
2019-08-20 12:43:08 +0200

    Create copy to main.py

---
+++ pyqt/copy to main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 14 15:56:45 2019
+
+@author: Hossain
+"""
+
+#copy to main.py
+
+import copy_file
+#import test
+
+ui_file = 'cricket_scorer.ui'
+
+copy_file.copy_to_main(ui_file)
+#test.copy_to_main(ui_file)
+
+#copy_file.copy_to_anaconda3(ui_file)
